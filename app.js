1754e5acd7edba70f948a4b6a21ac03aa35a06dc
Henri <HenriChabrand@users.noreply.github.com>
2020-02-28 10:16:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var bs = BinaryServer({server: server});
 var room = {};
 
 app.configure(function(){
-  app.set('port', process.argv[2] || 3000);
+  app.set('port', 3000);
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'jade');
 
